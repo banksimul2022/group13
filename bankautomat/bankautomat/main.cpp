70f429f3e5c2bf7f5578c927d27eb8d905fa7224
Jokuloinen <t0juve01@students.oamk.fi>
2022-04-03 18:25:17 +0300

    alkeet tilakoneelle exe mainissa

--- bankautomat/bankautomat/main.cpp
+++ bankautomat/bankautomat/main.cpp
@@ -1,11 +1,76 @@
 #include "mainwindow.h"
-
 #include <QApplication>
+#include <QDebug>
+#include <QThread>
+//global vars for our things
+bool stop=false;
+//char state=kortti;
+//active window
+//active ui
 
-int main(int argc, char *argv[])
-{
-    QApplication a(argc, argv);
+//comon window/ui functions
+void spawnWindow(){//malloc and spawn window with the related gui
+}
+void butcherWindow(){//dealloc a window and it's internal things
+}
+
+//restapi access
+void restHandler(int op, QString path){//op=operation, [0get,1push,2put,3delete]
+       qDebug() << op << path;
+}
+
+//signal slot thingy??? or my blocking latch/bus setup
+void rfidHandler(){
+}
+
+
+
+//state machine functions
+enum states{kortti, pin, tili};
+int state=kortti;
+void exit(QString msg, char msgType, bool kill){
+    qDebug() << kill << ' ' << msgType << ' ' << state << ' ' << '\t' << msg;
+    stop = kill;
+	state = kortti;
+}
+void sfKortti(){
     MainWindow w;
     w.show();
-    return a.exec();
+    state = pin;
+    QThread::sleep(1);
+    w.close();
+}
+void sfPin(){
+	//PinWindow w;
+	//w.show();
+    state = tili;
+	//w.close();
+}
+void sfTili(){
+	//TiliWindow w;
+	//w.show();
+    //state = kortti;
+    state = 48;
+    //w.close();
+}
+
+int main(int argc, char *argv[]){
+	QApplication a(argc, argv);
+    while(!stop){
+        qDebug() << state;
+        switch(state){
+			case kortti :
+				sfKortti();
+				break;
+			case pin :
+				sfPin();
+				break;
+			case tili :
+				sfTili();
+				break;
+			default :
+                exit("state non existent", 'f', true);
+		}
+	}
+	return a.exec();
 }
